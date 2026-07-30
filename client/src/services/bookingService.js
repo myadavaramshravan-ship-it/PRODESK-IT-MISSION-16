cb74c676daa2f6e66d2ae2bd6df9f3992651e8cf
myadavaramshravan-ship-it <myadavaramshravan@gmail.com>
2026-07-31 03:04:12 +0530

    fix booking api for production

--- client/src/services/bookingService.js
+++ client/src/services/bookingService.js
@@ -1,57 +1,50 @@
-import axios from "axios";
-const API = "http://localhost:5000/api/bookings";
+import api from "./api";
+
+
 export const createBooking = async(data)=>{
-    const token = localStorage.getItem("token");
-    const response = await axios.post(
-        API,
-        data,
-        {
-            headers:{
-                Authorization:`Bearer ${token}`
-            }
-        }
+
+    const response = await api.post(
+        "/api/bookings",
+        data
     );
+
     return response.data;
+
 };
+
+
 
 export const getBookings = async()=>{
-    const token = localStorage.getItem("token");
-    const response = await axios.get(
-        API,
-        {
-            headers:{
-                Authorization:`Bearer ${token}`
-            }
-        }
+
+    const response = await api.get(
+        "/api/bookings"
     );
+
     return response.data;
+
 };
+
+
 
 export const updateBooking = async(id,data)=>{
-    const token = localStorage.getItem("token");
-    const response = await axios.put(
-        `${API}/${id}`,
-        data,
-        {
-            headers:{
-                Authorization:`Bearer ${token}`
-            }
-        }
 
+    const response = await api.put(
+        `/api/bookings/${id}`,
+        data
     );
+
     return response.data;
+
 };
 
+
+
 export const deleteBooking = async(id)=>{
-    const token = localStorage.getItem("token");
-    const response = await axios.delete(
-        `${API}/${id}`,
-        {
-            headers:{
-                Authorization:`Bearer ${token}`
-            }
-        }
+
+    const response = await api.delete(
+        `/api/bookings/${id}`
     );
+
     return response.data;
 
 };
